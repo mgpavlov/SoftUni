eee559516932c327f407d97982d8a6102330b505
Mihail Pavlov <30524177+mgpavlov@users.noreply.github.com>
2019-02-02 12:56:18 +0200

    refactoring create Constants

---
+++ Java/Java Web/01.Java Web Development Basics - January 2019/04.Introduction To Java EE/Exercise/fdmc/src/main/java/fdmc/Constants.java
@@ -0,0 +1,15 @@
+package fdmc;
+
+public class Constants {
+
+    //Change Files Paths
+    public final static String INDEX_HTML_FILE_PATH = "D:\\SoftUni\\Java\\Java Web\\01.Java Web Development Basics - January 2019\\04.Introduction To Java EE\\Exercise\\fdmc\\src\\main\\resources\\views\\index.html";
+    public final static String CAT_CREATE_HTML_FILE_PATH = "D:\\SoftUni\\Java\\Java Web\\01.Java Web Development Basics - January 2019\\04.Introduction To Java EE\\Exercise\\fdmc\\src\\main\\resources\\views\\cat-create.html";
+    public final static String CAT_PROFILE_HTML_FILE_PATH = "D:\\SoftUni\\Java\\Java Web\\01.Java Web Development Basics - January 2019\\04.Introduction To Java EE\\Exercise\\fdmc\\src\\main\\resources\\views\\cat-profile.html";
+    public final static String ALL_CATS_HTML_FILE_PATH = "D:\\SoftUni\\Java\\Java Web\\01.Java Web Development Basics - January 2019\\04.Introduction To Java EE\\Exercise\\fdmc\\src\\main\\resources\\views\\cat-all.html";
+
+    public final static String THERE_ARE_NO_CATS_MESSAGE = "There are no cats. <a href=\"/cats/create\">Create some!</a>";
+    public final static String CAT_NAME_AND_DETAILS_URL = "<a href=\"/cats/profile?catName=%s\">%s</a><br />";
+    public final static String CAT_DETAILS_URL = "/cats/profile?catName=%s";
+
+}
